c40c9b224facd066f42f8630839c6e9864ac357a
encarnacao <cencarnacaoq@gmail.com>
2023-02-28 13:54:45 -0300

    rename files

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { userSchema, loginSchema } from "../schemas/schemas.js";
 import { validateRequest } from "../middlewares/validateSchemas.js";
-import { singUpConflict } from "../middlewares/checkConflicts.js";
+import { singUpConflict } from "../middlewares/authMiddlewares.js";
 import { singUp } from "../controllers/authControllers.js";
 
 const router = Router();
-
+router.post("/signin", validateRequest(loginSchema));
 router.post("/signup", validateRequest(userSchema), singUpConflict, singUp);
 
 export default router;
