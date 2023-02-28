8b2e0574ea9598fe6d7497060d96e04ff630de75
encarnacao <cencarnacaoq@gmail.com>
2023-02-28 12:48:46 -0300

    fix: adjust password length

--- src/schemas/schemas.js
+++ src/schemas/schemas.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const userSchema = Joi.object({
 	name: Joi.string().min(3).max(50).required(),
 	email: Joi.string().email().required(),
-	password: Joi.string().min(6).max(15).required(),
+	password: Joi.string().min(3).max(15).required(),
 	confirmPassword: Joi.any()
 		.valid(Joi.ref("password"))
 		.messages({ "any.only": "Passwords do not match" })
@@ -12,7 +12,7 @@ export const userSchema = Joi.object({
 
 export const loginSchema = Joi.object({
 	email: Joi.string().email().required(),
-	password: Joi.string().min(6).max(15).required(),
+	password: Joi.string().min(3).max(15).required(),
 });
 
 export const postSchema = Joi.object({
