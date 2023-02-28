fd2b82d8fef51f603d6491ea23823f17d98cea2c
encarnacao <cencarnacaoq@gmail.com>
2023-02-28 17:48:10 -0300

    feat: add short url validation through middleware

---
+++ src/middlewares/urlsMiddlewares.js
@@ -0,0 +1,20 @@
+import { db } from "../config/database.connect.js";
+
+async function validateShortUrl(req, res, next) {
+	const { shortUrl } = req.params;
+	try {
+		const url = await db.query(`SELECT * FROM links WHERE "shortUrl" = $1`, [
+			shortUrl,
+		]);
+		if (url.rowCount === 0) {
+			return res.status(404).send("Not Found");
+		}
+		res.locals.url = url.rows[0];
+		next();
+	} catch (e) {
+		console.log(e);
+		res.status(500).send("Internal Server Error");
+	}
+}
+
+export { validateShortUrl };
