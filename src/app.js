8539b069b9131a813053fac9d9ebc9eab3703c9f
encarnacao <cencarnacaoq@gmail.com>
2023-02-28 17:32:47 -0300

    feat: use url router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRouter from "./routers/authRouter.js";
+import urlRouter from "./routers/urlRouter.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(authRouter);
+app.use(urlRouter);
 
 app.get("/", (_, res) => {
 	res.send("Hello World!");
